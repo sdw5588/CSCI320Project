f387dd6d4920f584984fc44e37028de7a981365d
odm7341 <omcclu@gmail.com>
2020-10-08 18:21:12 -0400

    More Functions Done

--- src/main.py
+++ src/main.py
@@ -87,7 +87,8 @@ def registerUser():
     cursor.execute(sql, (id, fname, lname))
     conn.commit()
     print("...User added successfully!")
-    sleep(.5)
+    sleep(.7)
+    cursor.close()
     start()
 
 def getUserName():
@@ -104,6 +105,7 @@ def getUserName():
     name = cursor.fetchall()
 
     if len(name) == 1:
+        cursor.close()
         userMenu(id, name[0])
     else:
         print('Please enter a valid user_id')
@@ -115,7 +117,8 @@ def showUserMenu():
     print(' 0. Back')
     print(' 1. Add Tool')
     print(' 2. Edit Tool')
-    print(' 3. Add Collection')
+    print(' 3. View your Tools')
+    print(' 4. View your Collections')
     print(' -- -- -- -- -- -- -- -- -- -- ')
 
 
@@ -124,16 +127,24 @@ def userMenu(id, user_name):
     print('Hello,', user_name[0])
     showUserMenu()
     n = int(input('Enter option : '))
+    # exit
     if n == 0:
         os.system('cls')
         start()
+    # add tool
     elif n == 1:
         os.system('cls')
         addTool(id)
+    # edit tool
     elif n == 2:
         os.system('cls')
         editTool(id)
-        print(user_name[0])
+    # View my Tools
+    elif n == 3:
+        os.system('cls')
+    # View my collections
+    elif n == 4:
+        os.system('cls')
     else:
         os.system('cls')
         start()
@@ -226,6 +237,10 @@ def addTool(id):
         cursor.execute(sql, (id, barcode, buy_date))
 
     conn.commit()
+    cursor.close()
+
+    print('...Tool successfully added')
+    sleep(.7)
 
 def editTool(id):
     global conn
@@ -251,18 +266,19 @@ def editTool(id):
         print(tool[0], tool[1], sep='\t')
         barcodes.append(tool[0])
         tool_names.append(tool[1])
+    print(' -- -- ')
 
     while selected_tool == False:
         barcode = int(input('Enter the tool barcode : '))
 
         if barcode in barcodes:
             # Its actually their tool
+            cursor.close()
             toolEdit(id, barcode, tool_names[barcodes.index(barcode)])
             selected_tool = True
         else:
             #not their tool
             print('You dont own that tool, or it doesnt exist')
-
 
 
 
@@ -273,6 +289,8 @@ def showToolEdit():
     print(' 1. Change Name')
     print(' 2. Lend Tool')
     print(' 3. Add to Collection')
+    print(' 4. Add to Category')
+    print(' 5. Sell')
     print(' -- -- -- -- -- -- -- -- -- -- ')
 
 
@@ -285,13 +303,139 @@ def toolEdit(id, barcode, tool_name):
         os.system('cls')
     elif n == 1: # change name
         os.system('cls')
+        changeName(id, barcode, tool_name)
     elif n == 2: # Lend
         os.system('cls')
+        lend(id, barcode, tool_name)
     elif n == 3: # add to collection
         os.system('cls')
+        addToCollection(id, barcode, tool_name)
+    elif n == 4: # add to category
+        os.system('cls')
+        addToCategory(id, barcode, tool_name)
+    elif n == 5: # sell
+        os.system('cls')
+        sell(id, barcode, tool_name)
     else:
         os.system('cls')
         start()
+
+def changeName(id, barcode, tool_name):
+    global conn
+    cursor = conn.cursor()
+
+    new_name = input('Whats the new name for ' + tool_name + " : ")
+
+    sql ='''
+    UPDATE "tool"
+    SET "name" = %s
+    WHERE "barcode" = %s
+    '''
+    cursor.execute(sql, (new_name, barcode))
+
+    conn.commit()
+
+    print('...Update Successful')
+    sleep(.7)
+    cursor.close()
+
+def lend(id, barcode, tool_name):
+    ## TODO: Write the function
+    pass
+
+def addToCollection(id, barcode, tool_name):
+    global conn
+    cursor = conn.cursor()
+
+    # Get a list of all collections
+    sql = '''
+    SELECT "coll_name" FROM "collection"
+    '''
+    cursor.execute(sql)
+    all_colls = cursor.fetchall()
+
+    print(' -- Existing Collections are -- ')
+    coll_list = []
+    new_coll = True
+    for coll in all_colls:
+        coll_list.append(coll[0])
+        print(coll[0])
+    print(' -- -- ')
+
+    collection = input('\nEnter collection name\nIf that collection does not exist it will be made : ')
+
+    if collection in coll_list:
+        new_coll = False
+
+    if new_coll:
+        # add in new collection
+        sql ='''
+        INSERT INTO "collection" ("coll_name")
+        VALUES (%s)
+        '''
+        cursor.execute(sql, (collection,))
+
+    # add in collection relation
+    sql ='''
+    UPDATE "owns"
+    SET "collection" = %s
+    WHERE "user_id" = %s AND "barcode" = %s;
+    '''
+    cursor.execute(sql, (collection, id, barcode))
+    conn.commit()
+    print('...Successfully Added')
+    sleep(.7)
+    cursor.close()
+
+
+def addToCategory(id, barcode, tool_name):
+    global conn
+    cursor = conn.cursor()
+
+    # Get a list of all categories
+    # Also this is a copy of code in the Add tool func, because idk how to not do that
+    sql = '''
+    SELECT "cat_name" FROM "category"
+    '''
+    cursor.execute(sql)
+    all_cats = cursor.fetchall() # look at this cat --> (,,,)=(^..^)=(,,,)
+
+    print(' -- Existing Categories are -- ')
+    cat_list = []
+    new_cat = True
+    for cat in all_cats:
+        cat_list.append(cat[0])
+        print(cat[0])
+    print(' -- -- ')
+
+    category = input('\nEnter cat_name\nIf that category does not exist it will be made : ')
+
+    if category in cat_list:
+        new_cat = False
+
+    if new_cat:
+        # add in new category
+        sql ='''
+        INSERT INTO "category" ("cat_name")
+        VALUES (%s)
+        '''
+        cursor.execute(sql, (category,))
+
+    # add in category relation
+    sql ='''
+    INSERT INTO "is_in" ("cat_name", "barcode")
+    VALUES (%s, %s)
+    '''
+    cursor.execute(sql, (category, barcode))
+    conn.commit()
+    print('...Successfully Added')
+    sleep(.7)
+    cursor.close()
+
+
+def sell(id, barcode, tool_name):
+    ## TODO: Write the function
+    pass
 
 
 
